743c99a7709e6dad5ae3ed079a70ad366264cedd
Greg Harmon <gharm@google.com>
2018-04-04 10:41:09 -0700

    "list" command: check gcloud-configured project if it's not set via flag.

--- app/kubemci/cmd/list_test.go
+++ app/kubemci/cmd/list_test.go
@@ -15,27 +15,55 @@
 package cmd
 
 import (
+	"strings"
 	"testing"
+
+	"github.com/GoogleCloudPlatform/k8s-multicluster-ingress/app/kubemci/pkg/kubeutils"
+	"github.com/golang/glog"
 )
 
 func TestValidateListArgs(t *testing.T) {
+	// To start with, there's no gcloud GCP project set:
+	mockProject := ""
+	kubeutils.ExecuteCommand = func(args []string) (string, error) {
+		if strings.Join(args, " ") == "gcloud config get-value project" {
+			glog.V(2).Infof("Returning proj: [%s]", mockProject)
+			return mockProject, nil
+		}
+		return "", nil
+	}
+
 	// It should return an error with extra args.
 	options := listOptions{}
 	if err := validateListArgs(&options, []string{"arg1"}); err == nil {
 		t.Errorf("Expected error for non-empty args")
 	}
 
-	// It should return an error with missing project.
+	// It should return an error without project being set anywhere.
 	options = listOptions{}
 	if err := validateListArgs(&options, []string{}); err == nil {
-		t.Errorf("Expected error for non-empty args")
+		t.Errorf("Expected error for missing explicit project")
 	}
 
-	// validateListArgs should succeed with a project and empty args.
+	// validateListArgs should succeed with project flag and empty args.
 	options = listOptions{
 		GCPProject: "myGcpProject",
 	}
 	if err := validateListArgs(&options, []string{}); err != nil {
-		t.Errorf("unexpected error from validateListArgs: %s", err)
+		t.Errorf("unexpected error from validateListArgs with explicit project: %s", err)
 	}
+	if options.GCPProject != "myGcpProject" {
+		t.Errorf("unexpected project. Want: %s. Got:%s", "myGcpProject", options.GCPProject)
+	}
+
+	// validateListArgs should succeed with a project from gcloud and empty args.
+	mockProject = "gcloud-project"
+	options = listOptions{}
+	if err := validateListArgs(&options, []string{}); err != nil {
+		t.Errorf("unexpected error from validateListArgs with gcloud project: %s", err)
+	}
+	if options.GCPProject != mockProject {
+		t.Errorf("unexpected project. Want: %s. Got:%s", mockProject, options.GCPProject)
+	}
+
 }
